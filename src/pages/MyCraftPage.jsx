296a513c4c3d138893eb87ec7d7c055890ee04c9
Arafat1652 <arafathosen4042@gmail.com>
2024-04-29 00:54:33 +0600

    implemented toggle dark mode

--- src/pages/MyCraftPage.jsx
+++ src/pages/MyCraftPage.jsx
@@ -40,7 +40,7 @@ const MyCraftPage = () => {
   const handleDelete = (_id) => {
     Swal.fire({
       title: "Are you sure?",
-      text: "You won't be able to revert this!",
+      text: "You want to delete this!",
       icon: "warning",
       showCancelButton: true,
       confirmButtonColor: "#3085d6",
--- src/pages/Nav.jsx
+++ src/pages/Nav.jsx
@@ -10,20 +10,20 @@ const Nav = () => {
   const {user, logOut} = useContext(AuthContext)
   const [theme, setTheme] = useState('light')
 
-  const handleThemeToogle=e=>{
-    // console.log(e.target.checked)
-      if(e.target.checked){
-          setTheme( "dark")
-      }else{
-          setTheme('light')
-      }
-  }
-  useEffect(()=>{
-    localStorage.setItem('theme', theme)
-    const localTheme = localStorage.getItem('theme')
-    document.querySelector('html').setAttribute('data-theme', localTheme)
+  useEffect(() => {
+    const savedTheme = localStorage.getItem('theme');
+    if (savedTheme) {
+      setTheme(savedTheme);
+      document.querySelector('html').setAttribute('data-theme', savedTheme);
+    }
+  }, []);
 
-  },[theme])
+  const handleThemeToogle = (e) => {
+    const newTheme = e.target.checked ? 'dark' : 'light';
+    setTheme(newTheme);
+    localStorage.setItem('theme', newTheme);
+    document.querySelector('html').setAttribute('data-theme', newTheme);
+  };
 
   console.log(theme,'theme')
 
@@ -79,7 +79,12 @@ const handleLogOut =()=>{
     {/* toagle */}
 
     <label className="cursor-pointer mr-8 grid place-items-center">
-  <input onChange={handleThemeToogle}  type="checkbox" className="toggle theme-controller bg-base-content row-start-1 col-start-1 col-span-2"/>
+    <input
+              onChange={handleThemeToogle}
+              type="checkbox"
+              className="toggle theme-controller bg-base-content row-start-1 col-start-1 col-span-2"
+              checked={theme === 'dark'}
+            />
   <svg className="col-start-1 row-start-1 stroke-base-100 fill-base-100" xmlns="http://www.w3.org/2000/svg" width="14" height="14" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><circle cx="12" cy="12" r="5"/><path d="M12 1v2M12 21v2M4.2 4.2l1.4 1.4M18.4 18.4l1.4 1.4M1 12h2M21 12h2M4.2 19.8l1.4-1.4M18.4 5.6l1.4-1.4"/></svg>
   <svg className="col-start-2 row-start-1 stroke-base-100 fill-base-100" xmlns="http://www.w3.org/2000/svg" width="14" height="14" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><path d="M21 12.79A9 9 0 1 1 11.21 3 7 7 0 0 0 21 12.79z"></path></svg>
 </label>
